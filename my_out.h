e2a15c68fccf3c511398f5d39bf02f92ab00b899
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-23 18:02:01 -0300

    att

--- my_out.h
+++ my_out.h
@@ -1,14 +1,3 @@
-# key_obfuscate
-a key obfuscator generator that generate obfuscated keys for C code , that are harder to be reverse engineered.
-
-## Cli Usage
-for creating a key encryption,just run the command below:
-```shell
- KeyObfuscate --entry 'the key i want to encrypt' --seed  1234 --project_name 'my_project' --output 'my_out.h'
- ```
-this will generate a file called `my_out.h` with the key encrypted.
-
-```c 
 #ifndef my_project_get_key_h
 #define my_project_get_key_h
 #define my_projectkey_size 25
@@ -95,20 +84,3 @@ this will generate a file called `my_out.h` with the key encrypted.
 	}\
 
 #endif
-``` 
-so your main code will look like this:
-```c
-
-## Building from Scratch
-if you want to build the code from scracth  you need to have [Darwin](https://github.com/OUIsolutions/Darwin) 
-installed on versio **0.020** and **Docker** our **Podman** installed on your machine.
-After install all dependecies,clone the repo on your machine , than run:
-```shel
-arwin  run_blueprint build/ --mode folder lua_api lua_cli  amalgamate local_compilation alpine_static debian_static rpm_static windowsi32
-```
-
-## Building Testing 
-if you want just build for testing you can run  and test using the lua api
-```shell
-darwin  run_blueprint build/ --mode folder lua_api lua_cli 
-```
