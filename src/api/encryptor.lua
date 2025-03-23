e627d8fec6eb49f9bd9eef1583711bd72beecb7d
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-23 05:09:21 -0300

    att

--- src/api/encryptor.lua
+++ src/api/encryptor.lua
@@ -1,7 +1,14 @@
 
 public_key_obfuscate.create_encryptations = function(props)
-    for i = 1,100 do
-    local rand = private_key_obfuscate.rand_generate(6876+i,54755)
-    print(rand)
+    local rand =private_key_obfuscate.newRandonizer(119)
+    local total_higer = 0
+    for i = 1,10000 do
+        local num = rand.generate_num(1,100)
+        if num > 50 then
+            total_higer = total_higer + 1
+        end
+        print("num: "..num)
     end 
+    print("Total higer than 50: "..total_higer)
+    print("Total lower than 50: "..(10000 - total_higer))
 end 
--- src/api/randonizer.lua
+++ src/api/randonizer.lua
@@ -3,43 +3,43 @@ private_key_obfuscate.flor = function(value)
     return value - (value % 1)
 end
 
+private_key_obfuscate.ajust = function(value,min,max)
+    
+    while value > max or value < min do
+        if(value > max) then
+            value = value / 3.9
+        end
+        if(value < min) then
+            value = value * 3.5
+        end
+    end
+    return private_key_obfuscate.flor(value)
+end
+
 private_key_obfuscate.rand_generate = function(value,seed)
 
     local TOTAL_LOOPS = 20
     local MIN_LIMIT = 1000
-    local MAX_LIMIT = 10000
+    local MAX_LIMIT = 100000
     local result = value
     
     for i=1,TOTAL_LOOPS do
         result = result * (seed / i + seed) 
         result = result * result
-
-        while result > MAX_LIMIT or result < MIN_LIMIT do
-            
-            if(result > MAX_LIMIT) then
-                result = result / 3
-            end
-
-            if(result < MIN_LIMIT) then
-                result = result * 2
-            end
-        end
-
-
+        result = private_key_obfuscate.ajust(result,MIN_LIMIT,MAX_LIMIT)
     end
-    --make fits in the range
-    while result > MAX_LIMIT or result < MIN_LIMIT do
-        if(result > MAX_LIMIT) then
-            result = result / 2
-        end
-        if(result < MIN_LIMIT) then
-            result = result * 2
-        end
-    end
-
-    return private_key_obfuscate.flor(result)
+    return private_key_obfuscate.ajust(result,MIN_LIMIT,MAX_LIMIT)
 end 
+
 private_key_obfuscate.newRandonizer = function(seed)
     local self_obj = {}
-    self_obj.seed = seed
+    local seed = seed
+    local increment = 10
+    self_obj.generate_num = function(min,max)
+        increment = increment + 10
+        local num = private_key_obfuscate.rand_generate(increment,seed)
+        return private_key_obfuscate.ajust(num,min,max)
+    end 
+
+    return self_obj
 end
