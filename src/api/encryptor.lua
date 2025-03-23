b34262a35c38faf6d5015ae3cdf3b5531c117aff
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-23 04:23:32 -0300

    att

--- src/api/encryptor.lua
+++ src/api/encryptor.lua
@@ -1,6 +1,6 @@
 
 public_key_obfuscate.create_encryptations = function(props)
-    for i = 1,10 do
+    for i = 1,100 do
     local rand = private_key_obfuscate.rand_generate(6876+i,54755)
     print(rand)
     end 
